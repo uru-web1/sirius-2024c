093d24b7c3b0bf6dd8f3e85ea155bb86f60c2c21
ange1020 <angelina.30571079@uru.edu>
2024-10-23 22:34:18 -0400

    refactor: Modify SiriusLabel.html and SiriusLabel.js

--- Sirius/public/js/SiriusLabel.js
+++ Sirius/public/js/SiriusLabel.js
@@ -1,4 +1,4 @@
-import { SIRIUS_TYPES, SiriusElement, SIRIUS_ELEMENT } from "./SiriusElement.js";
+import {SIRIUS_TYPES, SiriusElement} from "./SiriusElement.js";
 import deepFreeze from "./utils/deep-freeze.js";
 
 /** Sirius label constants */
@@ -6,8 +6,8 @@ export const SIRIUS_LABEL = deepFreeze({
     NAME: "SiriusLabel",
     TAG: "sirius-label",
     ATTRIBUTES: {
-        CAPTION: { NAME: "caption", DEFAULT: "Valor", TYPE: SIRIUS_TYPES.STRING },
-        CAPTION_STYLE: { NAME: "caption-style", DEFAULT: null, TYPE: [SIRIUS_TYPES.OBJECT, SIRIUS_TYPES.STRING] },
+        CAPTION: {NAME: "caption", DEFAULT: "", TYPE: SIRIUS_TYPES.STRING},
+        TYPE: {NAME: "type", DEFAULT: "", TYPE: SIRIUS_TYPES.STRING},
     },
     CLASSES: {
         LABEL: 'label-container',
@@ -15,6 +15,7 @@ export const SIRIUS_LABEL = deepFreeze({
     }
 });
 
+/** Sirius class that represents a label component */
 export class SiriusLabel extends SiriusElement {
     /**
      * Create a Sirius label element
@@ -28,102 +29,40 @@ export class SiriusLabel extends SiriusElement {
             htmlAttributes: SIRIUS_LABEL.ATTRIBUTES,
             properties: props
         });
-
-        // Attach shadow DOM
-        this.attachShadow({ mode: "open" });
     }
 
     /** Get the template for the Sirius label
      * @returns {string} - Template
      * */
     #getTemplate() {
-
         return `<div class="${SIRIUS_LABEL.CLASSES.LABEL}">
                     <span class ="${SIRIUS_LABEL.CLASSES.CAPTION}">${this.#getCaption()}</span>
                 </div>`;
     }
-    #getCaption(){
+
+    /** Get the caption of the label
+     * @returns {*} - Label caption
+     */
+    #getCaption() {
         return this._attributes[SIRIUS_LABEL.ATTRIBUTES.CAPTION.NAME];
     }
-    #loadAttributes() {
-        
-        // Check if the element has attributes
-        if (!this._attributes)
-            this.logger.log("No attributes");
 
-        Object.keys(this._attributes).forEach(attributeName => {
-
-            // Get the attribute value
-            const attributeValue = this._attributes[attributeName]
-
-
-            // Check if the attribute value is null
-            if (!attributeValue) return;
-            
-            switch (attributeName) {
-                
-                case SIRIUS_ELEMENT.ATTRIBUTES.STYLE.NAME:
-
-                    if (typeof attributeValue === SIRIUS_TYPES.STRING) {
-                        this.labelElement.style.cssText = attributeValue;
-                        return;
-                    }
-
-                    for (let styleName in attributeValue) {
-                        this.labelElement.style[styleName] = attributeValue[styleName];
-                    }
-                    break;
-                    
-                case SIRIUS_LABEL.ATTRIBUTES.CAPTION_STYLE.NAME:
-
-                    if (typeof attributeValue === SIRIUS_TYPES.STRING) {
-                        this.captionElement.style.cssText = attributeValue;
-                        return;
-                    }
-
-                    for (let styleName in attributeValue) {
-                        this.captionElement.style[styleName] = attributeValue[styleName];
-                    }
-                break;
-
-                case SIRIUS_ELEMENT.ATTRIBUTES.EVENTS.NAME:
-                    // TO BE IMPLEMENTED
-                    break;
-
-                default:
-                    // this.logger.log(`Unregistered attribute: ${attributeName}`);
-                    break;
-            }
-        })
-    }
     /** Lifecycle method called when the component is connected to the DOM
      */
     async connectedCallback() {
-
-        
         // Create the CSS stylesheet and add it to the shadow DOM
-        await this.getCss(SIRIUS_LABEL.NAME);
-        this.shadowRoot.adoptedStyleSheets = [this._sheet];
+        await this._loadElementStyles()
 
         // Get HTML inner content
         const innerHTML = this.#getTemplate();
-        if (!innerHTML) {
-            this.logger.error('Failed to create template');
-            return;
-        }
 
         // Create the HTML template
-        await this.createTemplate(innerHTML);
+        await this._createTemplate(innerHTML);
 
         // Add label to the shadow DOM
-        this.labelElement = this._templateContent.firstChild;
-        this.captionElement = this.labelElement.firstElementChild;
-        this.shadowRoot.appendChild(this.labelElement);
-
-        // Load attributes
-        this.#loadAttributes();
+        this.elementContainer = this._templateContent.firstChild;
+        this.shadowRoot.appendChild(this.elementContainer);
     }
-
 }
 
 // Register custom element
